f40284cb7ce2dd1f0aa3f1ec46b59714f1bcf954
Sma922 <tbsmesh10@gmail.com>
2026-03-07 01:57:14 +0200

    Update project

--- student_image_search/settings.py
+++ student_image_search/settings.py
@@ -1,11 +1,13 @@
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-bf6ds8l-&voeadzv46!f!f#sy(ms)ylf0t4jojg(q4a)sm=n)"
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
